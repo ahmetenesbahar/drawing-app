b46ada0f678046b1388e17599dae71d04e7d78ed
ahmetenesbahar <enesbahar2303@gmail.com>
2022-11-03 12:10:35 +0300

    drawing functionality added

--- script.js
+++ script.js
@@ -2,7 +2,35 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 let size = 20;
+let isPressed = false;
 let color = "black";
+let x;
+let y;
+
+canvas.addEventListener("mousedown", (e) => {
+  isPressed = true;
+  x = e.offsetX;
+  y = e.offsetY;
+});
+
+canvas.addEventListener("mouseup", (e) => {
+  isPressed = false;
+  x = undefined;
+  y = undefined;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+
+    x = x2;
+    y = y2;
+  }
+});
 
 function drawCircle(x, y) {
   ctx.beginPath();
@@ -16,6 +44,7 @@ function drawLine(x1, y1, x2, y2) {
   ctx.beginPath();
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size * 2;
+  ctx.stroke();
 }
-
-drawCircle(100, 200);
